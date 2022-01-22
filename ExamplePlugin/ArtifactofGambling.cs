7efabecce576f45984d2b1c9d6cfe9f97a3e894b
dancinBear <46678410+dancinBear@users.noreply.github.com>
2022-01-21 22:28:54 -0500

    initial commit

---
+++ ExamplePlugin/ArtifactofGambling.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using RoR2;
+using static BerryMods.BerryMods;
+namespace BerryMods
+{
+	class ExampleArtifact : ArtifactBase
+	{
+		public override string ArtifactName => "Artifact of Gambling";
+		public override string ArtifactLangTokenName => "ARTIFACT_OF_GAMBLING";
+		public override string ArtifactDescription => "Adds a chance for items to be duplicated, as well as a chance to lose items.";
+		public override Sprite ArtifactEnabledIcon => MainAssets.LoadAsset<Sprite>("ArtifactOfGamblingOn.png");
+		public override Sprite ArtifactDisabledIcon => MainAssets.LoadAsset<Sprite>("ArtifactOfGamblingOff.png");
+		public override void Init(ConfigFile config)
+		{
+			CreateConfig(config);
+			CreateLang();
+			CreateArtifact();
+			Hooks();
+		}
+		private void CreateConfig(ConfigFile config)
+		{
+			//TimesToPrintMessageOnStart = config.Bind<int>("Artifact: " + ArtifactName, "Times to Print Message in Chat", 5, "How many times should a message be printed to the chat on run start?");
+		}
+		public override void Hooks()
+		{
+			Inventory.onServerItemGiven += ItemDupeMaybe;
+		}
+		private void ItemDupeMaybe(Inventory inventory, ItemIndex index, int n)
+        {
+			ItemDef itemDef = ItemCatalog.GetItemDef(index);
+			if (NetworkServer.active && ArtifactEnabled && itemDef.canRemove && !itemDef.hidden)
+            {
+				var weightedSelection = new WeightedSelection<System.Action>();
+				weightedSelection.AddChoice(() =>
+				{
+					inventory.GiveItem(itemDef);
+					Chat.AddMessage("You try your luck...");
+				}, 40f);
+				weightedSelection.AddChoice(() =>
+				{
+					inventory.RemoveItem(itemDef, 999);
+					Chat.AddMessage("Your luck fails you...");
+				}, 6.9f);
+				weightedSelection.AddChoice(() =>
+				{
+					int stackCount = inventory.GetItemCount(index);
+					inventory.GiveItem(itemDef, stackCount);
+					Chat.AddMessage("You hit the jackpot!");
+				}, 0.1f);
+				weightedSelection.AddChoice(() =>
+				{
+					//this is deliberately blank, nothing is happening here.
+				}, 53f);
+				System.Action chosenAction = weightedSelection.Evaluate(RoR2Application.rng.nextNormalizedFloat);
+				chosenAction();
+			}
+        }
+	}
+}
---
+++ ExamplePlugin/BerryMods.cs
@@ -0,0 +1,78 @@
+using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace BerryMods
+{
+	//This is an example plugin that can be put in BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+    //This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
+    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(R2API.R2API.PluginGUID)]
+	
+	//This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+	
+	//We will be using 3 modules from R2API: ItemAPI to add our item, ItemDropAPI to have our item drop ingame, and LanguageAPI to add our language tokens.
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(ItemDropAPI), nameof(LanguageAPI), nameof(ArtifactAPI))]
+	
+	//This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class BerryMods : BaseUnityPlugin
+	{
+        //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
+        //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Berry_";
+        public const string PluginName = "BerryMods";
+        public const string PluginVersion = "0.0.1";
+        public List<ArtifactBase> Artifacts = new List<ArtifactBase>();
+        public static AssetBundle MainAssets;
+
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            //Init our logging class so that we can properly log for debugging
+            Log.Init(Logger);
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BerryMods.modassets"))
+            {
+                MainAssets = AssetBundle.LoadFromStream(stream);
+            }
+
+            var ArtifactTypes = Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(ArtifactBase)));
+            foreach (var artifactType in ArtifactTypes)
+            {
+                ArtifactBase artifact = (ArtifactBase)Activator.CreateInstance(artifactType);
+                if (ValidateArtifact(artifact, Artifacts))
+                {
+                    artifact.Init(Config);
+                }
+            }
+
+            Log.LogInfo(nameof(Awake) + " done.");
+        }
+
+        public bool ValidateArtifact(ArtifactBase artifact, List<ArtifactBase> artifactList)
+        {
+            var enabled = Config.Bind<bool>("Artifact: " + artifact.ArtifactName, "Enable Artifact?", true, "Should this artifact appear for selection?").Value;
+            if (enabled)
+            {
+                artifactList.Add(artifact);
+            }
+            return enabled;
+        }
+
+        //The Update() method is run on every frame of the game.
+        private void Update()
+        {  
+        }
+    }
+}
